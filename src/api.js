d7662e6d0b185437eb14ac13e6ee8415fcee0b21
rivebot <rive4785@gmail.com>
2025-08-07 23:34:04 -0400

    Removed OVH

--- src/api.js
+++ src/api.js
@@ -1,11 +1,9 @@
 import { Musixmatch } from './musixmatch.js';
 import lrclib from './lrclib.js';
-import ovh from './ovh.js';
 const genius = require('./genius.js').default;
 
 export default {
 	Musixmatch,
 	lrclib,
 	genius,
-	ovh,
 };
