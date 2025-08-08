8e838aa2553b55fabd173ef043d80cb845c272c2
rivebot <rive4785@gmail.com>
2025-08-07 21:05:00 -0400

    Changed cache system to a .json

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,8 +1,13 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
+function ensureJsonExt(file) {
+    return file.endsWith('.json') ? file : `${file}.json`;
+}
+
 export async function readToken(file) {
+    const jsonFile = ensureJsonExt(file);
     try {
-        const data = await readFile(file, 'utf-8');
+        const data = await readFile(jsonFile, 'utf-8');
         return JSON.parse(data);
     } catch {
         return null;
@@ -10,5 +15,6 @@ export async function readToken(file) {
 }
 
 export async function saveToken(file, token, expires) {
-    await writeFile(file, JSON.stringify({ value: token, expires }), 'utf-8');
+    const jsonFile = ensureJsonExt(file);
+    await writeFile(jsonFile, JSON.stringify({ value: token, expires }), 'utf-8');
 }
